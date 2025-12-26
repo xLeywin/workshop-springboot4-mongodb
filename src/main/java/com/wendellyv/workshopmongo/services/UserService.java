4dc1a16a31eeebb6285887d43226ef32c88458f9
Wendell Ylan Viani <wendellylan@gmail.com>
2025-12-26 07:30:20 -0300

    User insertion with POST

--- src/main/java/com/wendellyv/workshopmongo/services/UserService.java
+++ src/main/java/com/wendellyv/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.wendellyv.workshopmongo.services;
 
 import com.wendellyv.workshopmongo.domain.User;
+import com.wendellyv.workshopmongo.dto.UserDTO;
 import com.wendellyv.workshopmongo.repository.UserRepository;
 import com.wendellyv.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
