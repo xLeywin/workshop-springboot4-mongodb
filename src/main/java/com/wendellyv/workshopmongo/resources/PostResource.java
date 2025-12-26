03a12cdbe1ec19647d21dc756cd48ac88028ece6
Wendell Ylan Viani <wendellylan@gmail.com>
2025-12-26 20:53:14 -0300

    Revert "Simple query with @Query"
    This reverts commit c899419718b7eeb245e4e9281cdcd9ace862deb4.

--- src/main/java/com/wendellyv/workshopmongo/resources/PostResource.java
+++ src/main/java/com/wendellyv/workshopmongo/resources/PostResource.java
@@ -6,14 +6,13 @@ import com.wendellyv.workshopmongo.dto.PostDTO;
 import com.wendellyv.workshopmongo.dto.UserDTO;
 import com.wendellyv.workshopmongo.services.PostService;
 import com.wendellyv.workshopmongo.services.UserService;
-import com.wendellyv.workshopmongo.services.util.URL;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-
-import java.util.Date;
+import java.net.URI;
+import java.net.URL;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -37,24 +36,5 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
-    }
-
-    @RequestMapping(value="/titlesearch", method=RequestMethod.GET)
-    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
-        text = URL.decodeParam(text);
-        List<Post> list = service.findByTitle(text);
-        return ResponseEntity.ok().body(list);
-    }
-
-    @RequestMapping(value="/fullsearch", method=RequestMethod.GET)
-    public ResponseEntity<List<Post>> fullSearch(
-            @RequestParam(value="text", defaultValue="") String text,
-            @RequestParam(value="minDate", defaultValue="") String minDate,
-            @RequestParam(value="maxDate", defaultValue="") String maxDate) {
-        text = URL.decodeParam(text);
-        Date min = URL.convertDate(minDate, new Date(0L));
-        Date max = URL.convertDate(maxDate, new Date());
-        List<Post> list = service.fullSearch(text, min, max);
-        return ResponseEntity.ok().body(list);
     }
 }
--- src/main/java/com/wendellyv/workshopmongo/services/PostService.java
+++ src/main/java/com/wendellyv/workshopmongo/services/PostService.java
@@ -1,13 +1,14 @@
 package com.wendellyv.workshopmongo.services;
 
 import com.wendellyv.workshopmongo.domain.Post;
+import com.wendellyv.workshopmongo.domain.User;
+import com.wendellyv.workshopmongo.dto.UserDTO;
 import com.wendellyv.workshopmongo.repository.PostRepository;
+import com.wendellyv.workshopmongo.repository.UserRepository;
 import com.wendellyv.workshopmongo.services.exception.ObjectNotFoundException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,15 +25,6 @@ public class PostService {
     public Post findById(String id) {
         Optional<Post> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
-    }
-
-    public List<Post> findByTitle(String text) {
-        return repo.searchTitle(text);
-    }
-
-    public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
-        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
-        return repo.fullSearch(text, minDate, maxDate);
     }
 }
 
