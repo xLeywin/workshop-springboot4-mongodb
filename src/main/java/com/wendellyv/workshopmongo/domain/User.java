56d50e612ea341ac3ea351fc89efdc1fad02eb8a
Wendell Ylan Viani <wendellylan@gmail.com>
2025-12-26 18:41:16 -0300

    Referencing user posts

--- src/main/java/com/wendellyv/workshopmongo/domain/User.java
+++ src/main/java/com/wendellyv/workshopmongo/domain/User.java
@@ -1,8 +1,11 @@
 package com.wendellyv.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "user")
@@ -12,6 +15,9 @@ public class User implements Serializable {
     private String id;
     private String name;
     private String email;
+
+    @DBRef(lazy = true) // Does not automatically load all posts when recovering a user from the database
+    private List<Post> posts = new ArrayList<>();
 
     public User() {
     }
@@ -45,6 +51,14 @@ public class User implements Serializable {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
     }
 
     @Override
