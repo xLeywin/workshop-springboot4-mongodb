28e310668de769d327b116987ade9a3447ec782b
Wendell Ylan Viani <103383355+xLeywin@users.noreply.github.com>
2025-12-26 21:20:24 -0300

    Update PostService.java

--- src/main/java/com/wendellyv/workshopmongo/services/PostService.java
+++ src/main/java/com/wendellyv/workshopmongo/services/PostService.java
@@ -9,6 +9,7 @@ import com.wendellyv.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,5 +27,13 @@ public class PostService {
         Optional<Post> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
     }
-}
 
+    public List<Post> findByTitle(String text) {
+        return repo.searchTitle(text);
+    }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return repo.fullSearch(text, minDate, maxDate);
+    }
+}
